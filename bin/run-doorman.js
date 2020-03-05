c53d66d3910bd6d609c7550a3cfa5fb7304b8476
Jeremy Berman <jerber@sas.upenn.edu>
2020-03-05 17:12:33 -0500

    adding bin and pack first one

---
+++ bin/run-doorman.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+var shell = require('shelljs');
+const fs = require('fs');
+const pkg = require('../package.json');
+
+const givenProjectId = process.argv[2];
+const projectId = givenProjectId || process.env.FIREBASE_PROJECT_ID;
+console.log('using project id: ', projectId, 'env', givenProjectId, 'global', process.env.FIREBASE_PROJECT_ID);
+
+console.log('now logging shit, this is version', pkg.version);
+
+const OUTPUT_FILE = 'interactive';
+
+shell.exec('npm install -g firebase-tools && npm install -g firebase-functions@latest firebase-admin@latest --save');
+
+shell.exec(`firebase login:ci --interactive > ${OUTPUT_FILE}`); //TODO put back after testing
+
+const inter = fs.readFileSync(OUTPUT_FILE, 'utf8');
+let token = inter.substring(inter.lastIndexOf('1//'));
+token = token.substring(0, token.indexOf('[') - 1);
+token = token.trim().replace(/\r?\n|\r/g, '');
+console.log(`<<<${token}>>>`);
+
+const REPLACE_TEXT = '<your-project-name>';
+
+shell.exec('rm -r DoormanDownload');
+
+shell.exec('git clone https://github.com/jerber/DoormanDownload.git');
+
+shell.exec('pwd && cd DoormanDownload/functions && npm i');
+
+try {
+	const data = fs.readFileSync('DoormanDownload/.firebaserc_before', 'utf8');
+	const afterData = data.replace(REPLACE_TEXT, projectId);
+	fs.writeFileSync('DoormanDownload/.firebaserc', afterData);
+} catch (err) {
+	console.error(err);
+}
+// shell.exec(`sed 's/${replaceText}/${projectId}/' DoormanDownload/.firebaserc_before > DoormanDownload/.firebaserc`);
+
+shell.exec(`pwd && cd DoormanDownload/functions && firebase deploy --token "${token}" --only functions:doormanPhoneLogic`);
